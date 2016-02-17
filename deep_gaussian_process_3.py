8a143e8c07b784bfe5887ecf00312e65dc3f83d0
keyonvafa <keyonvafa@college.harvard.edu>
2016-02-17 00:19:01 -0500

    Includes smart initializations

--- deep_gaussian_process_3.py
+++ deep_gaussian_process_3.py
@@ -10,8 +10,7 @@ from autograd.numpy.linalg import solve
 import autograd.scipy.stats.multivariate_normal as mvn
 from builtins import range
 from autograd.scipy.misc import logsumexp
-
-from gaussian_process import rbf_covariance
+import itertools
 
 def build_step_function_dataset(D=1, n_data=40, noise_std=0.1):
     rs = npr.RandomState(0)
@@ -19,6 +18,30 @@ def build_step_function_dataset(D=1, n_data=40, noise_std=0.1):
     targets = np.sign(inputs) + rs.randn(n_data) * noise_std
     inputs  = inputs.reshape((len(inputs), D))
     return inputs, targets
+
+def build_toy_dataset(D=1, n_data=20, noise_std=0.1):
+    rs = npr.RandomState(0)
+    inputs  = np.concatenate([np.linspace(0, 3, num=n_data/2),
+                              np.linspace(6, 8, num=n_data/2)])
+    targets = (np.cos(inputs) + rs.randn(n_data) * noise_std) / 2.0
+    inputs = (inputs - 4.0) / 2.0
+    inputs  = inputs.reshape((len(inputs), D))
+    return inputs, targets
+
+def build_parabola(D=1, n_data=20, noise_std=0.1):
+    rs = npr.RandomState(0)
+    inputs  = np.linspace(-4, 4, num=n_data)
+    targets = inputs ** 2 + rs.randn(n_data) * noise_std
+    inputs  = inputs.reshape((len(inputs), D))
+    return inputs, targets
+
+def rbf_covariance(kernel_params, x, xp):
+    output_scale = np.exp(kernel_params[0])
+    lengthscales = np.exp(kernel_params[1:])
+    diffs = np.expand_dims(x /lengthscales, 1)\
+          - np.expand_dims(xp/lengthscales, 0)
+    return output_scale * np.exp(-0.5 * np.sum(diffs**2, axis=2)) # median across all data points of np.sum(diffs**2, axis=2) should be 1
+
 
 def make_gp_funs(cov_func, num_cov_params):
     """Functions that perform Gaussian process regression.
@@ -55,7 +78,7 @@ def build_deep_gp(input_dimension, hidden_dimension, n_layers, covariance_functi
     num_params_each_layer, predict_layer_funcs, predict_funcs_with_noise = zip(*layer_details)
 
     # Psuedo params defined as X0, y0 (inducing points). We have 10 of these for each layer    
-    num_pseudo_params = 10
+    num_pseudo_params = 5
     total_num_params = sum(num_params_each_layer) + n_layers*2*num_pseudo_params
 
     def unpack_all_params(all_params):
@@ -66,9 +89,19 @@ def build_deep_gp(input_dimension, hidden_dimension, n_layers, covariance_functi
         y0 = np.array_split(y0, n_layers)
         return layer_params, x0, y0
 
-    def sample_from_mvn(mu, sigma):
+    def pack_all_params(layer_params, x0, y0):
+        all_params = np.ndarray.flatten(np.array(layer_params))
+        #print(np.ndarray.flatten(np.array(x0)).shape)
+        #print(all_params.shape)
+        #print(np.concatenate([np.ndarray.flatten(np.array(x0)),all_params]))
+        all_params = np.concatenate([all_params, np.ndarray.flatten(np.array(x0))])
+        all_params = np.concatenate([all_params, np.ndarray.flatten(np.array(y0))])
+        return all_params
+
+
+    def sample_from_mvn(mu, sigma): # make sure we return 2d, also make sure data is 2d
         rs = npr.RandomState(0)
-        return np.dot(np.linalg.cholesky(sigma+1e-6*np.eye(len(sigma))),rs.randn(len(mu)))+mu 
+        return np.dot(np.linalg.cholesky(sigma+1e-6*np.eye(len(sigma))),rs.randn(len(sigma)))+mu 
 
     def sample_from_deep_gp(all_params, X, with_noise = False):
         predict = predict_funcs_with_noise if with_noise else predict_layer_funcs
@@ -83,32 +116,41 @@ def build_deep_gp(input_dimension, hidden_dimension, n_layers, covariance_functi
         h_2_mean, h_2_cov = predict[n_layers-1](layer_params[n_layers-1],np.atleast_2d(x0[n_layers-1]).T,y0[n_layers-1],X_star)
         return h_2_mean,h_2_cov
 
+    def squared_error(all_params):
+        n_samples = 1
+        samples = np.array([sample_from_deep_gp(all_params, X, False)[0] for i in xrange(n_samples)])
+        return np.mean((y - np.mean(samples,axis = 0)) ** 2)
+
     def log_likelihood(all_params): 
-        n_samples = 10
-        # print("Layer 1 Noise Scale", np.exp(layer1_params[1]) + 0.001)
-        # print("Layer 2 Noise Scale", np.exp(layer2_params[1]) + 0.001)
+        n_samples = 1
+        layer_params, x0, y0 = unpack_all_params(all_params)
+        #print("Layer 1 Noise Scale", np.exp(layer_params[0][1]) + 0.001)
+        #print("Layer 2 Noise Scale", np.exp(layer_params[1][1]) + 0.001)
+        #print("Layer 2 Mean: ", layer_params[1][0])
         # It looks like the layer 2 noise scale is going toward 0
         samples = [sample_from_deep_gp(all_params, X, True) for i in xrange(n_samples)]
         return logsumexp(np.array([mvn.logpdf(y,sample[0],sample[1]+1e-6*np.eye(len(sample[1]))) for sample in samples])) - np.log(n_samples) 
 
-    return total_num_params, log_likelihood, sample_from_deep_gp, unpack_all_params, predict_layer_funcs
+    return total_num_params, log_likelihood, sample_from_deep_gp, unpack_all_params, predict_layer_funcs, squared_error, pack_all_params
 
 if __name__ == '__main__':
 
     n_data = 20
     input_dimension = 1
     hidden_dimension = 1
-    n_layers = 2
-    X, y = build_step_function_dataset(D=input_dimension, n_data=n_data)
+    n_layers = 3
+    X, y = build_step_function_dataset(D=input_dimension, n_data=20)
+    #X, y = build_parabola(D = input_dimension, n_data = 10)
 
-    total_num_params, log_likelihood, sample_from_deep_gp, unpack_all_params, predict_layer_funcs = \
+    total_num_params, log_likelihood, sample_from_deep_gp, unpack_all_params, predict_layer_funcs, squared_error, pack_all_params = \
         build_deep_gp(input_dimension, hidden_dimension, n_layers, rbf_covariance)
 
     # Set up figure.
     fig = plt.figure(figsize=(12,8), facecolor='white')
-    ax_end_to_end = fig.add_subplot(311, frameon=False)
-    ax_x_to_h = fig.add_subplot(312, frameon=False)
-    ax_h_to_y = fig.add_subplot(313, frameon=False)
+    ax_first = fig.add_subplot(411, frameon=False)
+    ax_end_to_end = fig.add_subplot(412, frameon=False)
+    ax_x_to_h = fig.add_subplot(413, frameon=False)
+    ax_h_to_y = fig.add_subplot(414, frameon=False)
     plt.show(block=False)
 
 
@@ -118,20 +160,20 @@ if __name__ == '__main__':
         
         ax.cla()
         rs = npr.RandomState(0)
-        n_samples = 10
+        n_samples = 1
         
         sampled_means_and_covs = [sample_from_deep_gp(params, plot_xs) for i in xrange(n_samples)]
         sampled_means, sampled_covs = zip(*sampled_means_and_covs)
         avg_pred_mean = np.mean(sampled_means, axis = 0)
         avg_pred_cov = np.mean(sampled_covs, axis = 0)
         marg_std = np.sqrt(np.diag(avg_pred_cov))
-        ax.fill(np.concatenate([plot_xs, plot_xs[::-1]]),
-        np.concatenate([avg_pred_mean - 1.96 * marg_std,
-                       (avg_pred_mean + 1.96 * marg_std)[::-1]]),
-                       alpha=.15, fc='Blue', ec='None')
-        ax.plot(plot_xs, avg_pred_mean, 'b')
+        #ax.fill(np.concatenate([plot_xs, plot_xs[::-1]]),
+        #np.concatenate([avg_pred_mean - 1.96 * marg_std,
+        #               (avg_pred_mean + 1.96 * marg_std)[::-1]]),
+        #               alpha=.15, fc='Blue', ec='None')
+        #ax.plot(plot_xs, avg_pred_mean, 'b')
 
-        sampled_funcs = np.array([rs.multivariate_normal(*sample) for sample in sampled_means_and_covs])
+        sampled_funcs = np.array([rs.multivariate_normal(sample[0], sample[1]) for sample in sampled_means_and_covs])
         ax.plot(plot_xs,sampled_funcs.T)
         ax.plot(X, y, 'kx')
         #ax.set_ylim([-1.5,1.5])
@@ -143,15 +185,15 @@ if __name__ == '__main__':
     def plot_gp(ax, X, y, pred_mean, pred_cov, plot_xs):
         ax.cla()
         marg_std = np.sqrt(np.diag(pred_cov))
-        ax.plot(plot_xs, pred_mean, 'b')
-        ax.fill(np.concatenate([plot_xs, plot_xs[::-1]]),
-                np.concatenate([pred_mean - 1.96 * marg_std,
-                               (pred_mean + 1.96 * marg_std)[::-1]]),
-                alpha=.15, fc='Blue', ec='None')
+        #ax.plot(plot_xs, pred_mean, 'b')
+        #ax.fill(np.concatenate([plot_xs, plot_xs[::-1]]),
+        #        np.concatenate([pred_mean - 1.96 * marg_std,
+        #                       (pred_mean + 1.96 * marg_std)[::-1]]),
+        #        alpha=.15, fc='Blue', ec='None')
 
         # Show samples from posterior.
         rs = npr.RandomState(0)
-        sampled_funcs = rs.multivariate_normal(pred_mean, pred_cov, size=10)
+        sampled_funcs = rs.multivariate_normal(pred_mean, pred_cov, size=1)
         ax.plot(plot_xs, sampled_funcs.T)
         ax.plot(X, y, 'kx')
         #ax.set_ylim([-1.5, 1.5])
@@ -159,7 +201,7 @@ if __name__ == '__main__':
         ax.set_yticks([])
 
     def callback(params):
-        print("Log likelihood {}".format(-objective(params)))
+        print("Log likelihood {}, Squared Error {}".format(-objective(params),squared_error(params)))
         layer_params, x0, y0 = unpack_all_params(params)
 
         # Show posterior marginals.
@@ -180,14 +222,32 @@ if __name__ == '__main__':
 
     # Initialize covariance parameters and hiddens.
     rs = npr.RandomState(1234)
-    init_params = .1 * rs.randn(total_num_params)
+    init_params = .1 * rs.randn(total_num_params) # change initialization (maybe initializing by sampling from our training data), length scale is median of Euclidean distances between data points
+    # initialize pseudo x0 by subsampling, l by the median distance thing, outputs at the same value as the inputs for each layer
 
     print("Optimizing covariance parameters...")
     objective = lambda params: -log_likelihood(params)
+    layer_params, x0, y0 = unpack_all_params(init_params)
+    
+    pairs = itertools.combinations(X, 2)
+    dists = np.array([np.linalg.norm(np.array([p1])- np.array([p2])) for p1,p2 in pairs])
+    layer_params[0][3] = np.log(np.median(dists))
+    x0[0] = np.ndarray.flatten(np.array(X)[rs.choice(len(X), 5, replace=False),:])
+    y0[0] = x0[0]
+    
+    y0[1] = x0[1]
+    layer_params[1][3] = np.log(1)
+    
+    y0[2] = x0[2]
+    layer_params[2][3] = np.log(1)
+    init_params = pack_all_params(layer_params, x0, y0)
+
+    plot_xs = np.reshape(np.linspace(-5, 5, 300), (300,1))
+    plot_full_gp(ax_first, init_params, plot_xs)
+    ax_first.set_title("Initial full predictions")
+
+
     cov_params = minimize(value_and_grad(objective), init_params, jac=True,
                           method='BFGS', callback=callback)
-    print ("Params: ", init_params)
-    print("Grad: ", grad(my_objective)(init_params))
-
 
     plt.pause(10.0)
